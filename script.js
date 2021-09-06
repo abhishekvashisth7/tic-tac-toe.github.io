6c44e9ed0b7d419a8886af4e04034c39c0855a13
abhishekvashisth7 <abhishekvashisth027@gmail.com>
2021-09-06 14:24:50 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,129 @@
+const selectBox = document.querySelector(".select-box"),
+selectXBtn = selectBox.querySelector(".playerX"),
+selectOBtn = selectBox.querySelector(".playerO"),
+playBoard = document.querySelector(".play-board"),
+allBox = document.querySelectorAll("section span"),
+players = document.querySelector(".players"),
+resultBox = document.querySelector(".result-box"),
+wonText = resultBox.querySelector(".won-text"),
+replayBtn = resultBox.querySelector("button");
+
+
+window.onload = () => {//once window loaded
+    
+    for(let i=0; i < allBox.length;i++){  //add onclick attribute in all available section's spans
+        allBox[i].setAttribute("onclick","clickedBox(this)");
+    }
+    
+    selectXBtn.onclick = ()=>{
+        selectBox.classList.add("hide"); //hide the select box on playerX button clicked
+        playBoard.classList.add("show"); //show the playboard section on playerX button clicked
+        
+    }
+    selectOBtn.onclick = ()=>{
+        selectBox.classList.add("hide"); //hide the select box on playerO button clicked
+        playBoard.classList.add("show"); //show the playboard section on playerO button clicked
+        players.setAttribute("class","players active player");  //adding third class name in active player
+    }
+}
+
+let playerXIcon = "fas fa-times"; //class name of fontawesome cross icon
+let playerOIcon = "far fa-circle"; //class name of fontawesome circle icon
+let playerSign = "X";//suppose player will be X
+let runBot = true;
+
+
+function clickedBox(element){ 
+    if(players.classList.contains("player")){ //if players element contains player
+        element.innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside user clicked element 
+        players.classList.add("active");
+        playerSign = "O"; // if player will be O then we'll change the sign
+        element.setAttribute("id",playerSign)
+    }
+    else{
+        element.innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside user clicked element 
+        players.classList.add("active");
+        element.setAttribute("id",playerSign);
+    }
+    selectWinner();
+    element.style.pointerEvents = "none";
+    playBoard.style.pointerEvents = "none";
+    let randomDelayTime = ((Math.random() * 1000) + 200).toFixed(); // generating random time delay
+    
+    setTimeout(() => {
+        bot(runBot); //calling bot function
+    },randomDelayTime);
+}
+
+//bot click function
+function bot(runBot){
+    if(runBot){
+        playerSign = "O";
+        let array = [];     //creating empty array ... we'll store unselected box index in this array 
+        for (let i = 0; i < allBox.length; i++) { 
+            if(allBox[i].childElementCount == 0){ //if span has no any child element
+                array.push(i); //inserting unclicked or unselected boxes inside array means that span has no childern
+            }
+        }
+        let randomBox = array[Math.floor(Math.random()*array.length)]; //getting random index from array
+        if(array.length > 0){
+            if(players.classList.contains("player")){ //if players element contains player
+                allBox[randomBox].innerHTML = `<i class="${playerXIcon}"></i>`; //adding cross icon tag inside user clicked element 
+                players.classList.remove("active");
+                playerSign = "X";
+                allBox[randomBox].setAttribute("id",playerSign);
+            }
+            else{
+                allBox[randomBox].innerHTML = `<i class="${playerOIcon}"></i>`; //adding circle icon tag inside user clicked element 
+                players.classList.remove("active");
+                allBox[randomBox].setAttribute("id",playerSign);
+            }
+            selectWinner();
+        }
+
+        allBox[randomBox].style.pointerEvents = "none";
+        playBoard.style.pointerEvents = "auto";
+        playerSign = "X";
+    }
+
+}
+
+//let work on select the winner
+function getClass(idname){  
+    return document.querySelector(".box" + idname ).id// returnong id name
+}
+
+function checkClass(val1 , val2 , val3 , sign){
+    if(getClass(val1) == sign && getClass(val2) == sign && getClass(val3) == sign){
+        return true;
+    }
+}
+
+function selectWinner(){
+    if(checkClass(1,2,3, playerSign) || checkClass(4,5,6, playerSign) || checkClass(7,8,9, playerSign) || checkClass(1,4,7, playerSign) || checkClass(2,5,8, playerSign) || checkClass(3,6,9, playerSign) || checkClass(1,5,9, playerSign) || checkClass(3,5,7, playerSign)){
+        runBot = false;
+        bot(runBot);
+        setTimeout(() => {
+            playBoard.classList.remove("show");
+            resultBox.classList.add("show");
+        }, 700);
+        wonText.innerHTML = `Player <p>${playerSign}</p> won the game`;
+    }
+
+    else{
+        //if match has drawn
+        if(getClass(1) != "" && getClass(2) && getClass(3) != "" && getClass(4) != "" && getClass(5) != "" && getClass(6) != "" && getClass(7) != "" && getClass(8) != "" && getClass(9) != ""){
+            runBot = false;
+            bot(runBot);
+            setTimeout(() => {
+                playBoard.classList.remove("show");
+                resultBox.classList.add("show");
+            }, 700);
+            wonText.innerHTML = `Match has been drawn`;
+        }
+    }
+}
+
+replayBtn.onclick = () =>{
+    window.location.reload(); // reload the current page
+}
